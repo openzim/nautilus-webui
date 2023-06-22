5610460bd3d957e88f934d6ce72616cb5160a17d
Fledge Shiu <xzk0701@gmail.com>
2023-06-22 08:40:03 -0400

    Add setting cookie.

--- backend/src/backend/routes/users.py
+++ backend/src/backend/routes/users.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from httpx import codes
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
@@ -21,10 +21,13 @@ class UserModel(BaseModel):
 
 
 @router.post("", response_model=UserModel, status_code=codes.CREATED)
-async def create_user(session: Session = Depends(gen_session)) -> UserModel:
+async def create_user(
+    response: Response, session: Session = Depends(gen_session)
+) -> UserModel:
     """Post this endpoint to create a user."""
     new_user = User(created_on=datetime.utcnow())
     session.add(new_user)
     session.flush()
     session.refresh(new_user)
+    response.set_cookie(key="user_id", value=str(new_user.id))
     return UserModel.from_orm(new_user)
