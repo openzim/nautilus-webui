dda3a1e0fba37547eef0dee3c79bdea0fc326b5b
Fledge Shiu <xzk0701@gmail.com>
2023-07-31 16:22:19 -0400

    Add DropToStartField.

---
+++ frontend/src/components/DropToStartField.vue
@@ -0,0 +1,25 @@
+<template>
+  <TheSlogan />
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="card border-3 border-3 rounded-3 drop">
+        <div class="card-body d-flex justify-content-center align-items-center">
+          <h3 class="card-title">Drop File to Start!</h3>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-md-center text-secondary">
+      Agree to our Terms of Service and Privacy & Cookie Statement.
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.drop {
+  width: 25em;
+  height: 10em;
+  border-style: dashed;
+  border-color: var(--main-color);
+  color: var(--main-color);
+}
+</style>
