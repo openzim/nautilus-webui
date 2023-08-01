0c1058380f26019b298f4095137e3bacbc176bd3
Fledge Shiu <xzk0701@gmail.com>
2023-08-01 11:18:46 -0400

    Add front-page.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheSlogan from '@/components/TheSlogan.vue'
+import DropToStartField from '@/components/DropToStartField.vue'
+import TheFAQs from '@/components/TheFAQs.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="mt-5">
+    <TheSlogan />
+    <div class="mt-5">
+      <DropToStartField />
+    </div>
+    <div class="mt-5">
+      <TheFAQs />
+    </div>
+  </div>
 </template>
+
+<style scoped></style>
