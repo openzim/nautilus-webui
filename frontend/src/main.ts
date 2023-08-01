c370575cb283fa46c1cce5ff82fbd97e8e5cfcdd
Fledge Shiu <xzk0701@gmail.com>
2023-08-01 10:43:34 -0400

    Add TheFAQs component.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,12 @@ import 'bootstrap'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faPlus, faMinus)
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +18,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
