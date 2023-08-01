afa74d0fcfdefb185a12000abf78b3119ded1ea9
Fledge Shiu <xzk0701@gmail.com>
2023-08-01 16:16:41 -0400

    Remove ununsed code.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
 import FooterComponetns from './components/FooterComponents.vue'
 import HomeView from './views/HomeView.vue'
 </script>
