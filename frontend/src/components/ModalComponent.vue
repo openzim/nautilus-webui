489be6b8ded7d02a3837ba57757b9e18be697230
Fledge Shiu <xzk0701@gmail.com>
2023-08-15 09:34:14 -0400

    Let modal dialog scrollable.

--- frontend/src/components/ModalComponent.vue
+++ frontend/src/components/ModalComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="modal fade show" tabindex="-1" ref="modal">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-scrollable">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5">{{ props.title }}</h1>
