fb5251d285ef02f1e4adcecae3fb68381365da73
Fledge Shiu <xzk0701@gmail.com>
2023-08-18 11:20:40 -0400

    Add Contact and FAQ pages.

--- frontend/src/components/SideBarComponent.vue
+++ frontend/src/components/SideBarComponent.vue
@@ -2,25 +2,17 @@
   <div class="d-flex flex-column" style="height: 100%">
     <div class="d-flex justify-content-between mt-2 border-bottom border-2 border-white">
       <div class="fw-bold fs-4 text-light">Collection:</div>
-      <button
-        type="button"
-        class="btn fw-bold fs-5 text-light p-1"
-        @click.prevent="createAndUpdateProject"
-      >
+      <button type="button" class="btn fw-bold fs-5 text-light p-1" @click.prevent="createAndUpdateProject">
         <font-awesome-icon :icon="['fas', 'plus']" />
       </button>
     </div>
     <div class="flex-grow-1 py-2 overflow-x-auto">
-      <ProjectColumnComponent
-        v-for="project in projects"
-        :key="project.id"
-        :project="project"
-        @delete-project="removeProjectFromList"
-      />
+      <ProjectColumnComponent v-for="project in projects" :key="project.id" :project="project"
+        @delete-project="removeProjectFromList" />
     </div>
     <div class="border-top border-2 border-white py-2">
-      <p class="text-light">FAQ</p>
-      <p class="text-light">Contact</p>
+      <p class="text-light"><router-link to="/faq" class="link-light">FAQ</router-link></p>
+      <p class="text-light"><router-link to="/contact" class="link-light">Contact</router-link></p>
     </div>
   </div>
 </template>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,6 +3,8 @@ import StartView from '@/views/StartView.vue'
 import TermsOfService from '@/views/TermsOfService.vue'
 import CollectionsView from '@/views/CollectionsView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import FrequentlyAskedQuestions from '@/components/FrequentlyAskedQuestions.vue'
+import ContactPage from '@/components/ContactPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -25,6 +27,16 @@ const router = createRouter({
       path: '/terms-of-service',
       name: 'TermsOfService',
       component: TermsOfService
+    },
+    {
+      path: '/faq',
+      name: 'faq',
+      component: FrequentlyAskedQuestions
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactPage
     }
   ]
 })
