2af9910f293477bcefdc4c2d295640ed1ce61c9e
Fledge Shiu <xzk0701@gmail.com>
2023-08-23 13:08:51 -0400

    Add timeout to redis connection.

--- backend/api/redis.py
+++ backend/api/redis.py
@@ -3,5 +3,5 @@ from rq import Queue
 from api.constants import constants
 from redis import Redis
 
-redis_conn = Redis.from_url(constants.redis_uri)
+redis_conn = Redis.from_url(constants.redis_uri, socket_timeout=500)
 task_queue = Queue(constants.channel_name, connection=redis_conn)
