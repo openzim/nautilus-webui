2d1a1db8df1be1958b9079946e88df8c91c8a5d4
renaud gaudin <reg@rskg.org>
2024-05-07 16:11:23 +0000

    fixed code fmt

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -1,7 +1,11 @@
 <template>
   <footer class="footer d-flex justify-content-between mt-auto px-3 py-1 text-muted border-top">
     <div class="">
-      Powered by <a href="https://github.com/openzim/nautilus" target="_blank" class="link-secondary">Nautilus</a> &
+      Powered by
+      <a href="https://github.com/openzim/nautilus" target="_blank" class="link-secondary"
+        >Nautilus</a
+      >
+      &
       <a href="https://kiwix.org" target="_blank" class="link-secondary">Kiwix</a>
     </div>
     <div class="">Copyright {{ new Date().getFullYear() }}</div>
@@ -9,7 +13,7 @@
 </template>
 
 <style type="text/css">
-  .link-secondary:hover {
-    cursor: pointer;
-  }
+.link-secondary:hover {
+  cursor: pointer;
+}
 </style>
