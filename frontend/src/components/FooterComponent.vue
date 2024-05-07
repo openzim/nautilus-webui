6680f54b47eddf88ea34e1783e8622ebd568cbd5
renaud gaudin <reg@rskg.org>
2024-05-07 16:03:05 +0000

    Fixed #63: footer links _blank and cursor

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -1,9 +1,15 @@
 <template>
   <footer class="footer d-flex justify-content-between mt-auto px-3 py-1 text-muted border-top">
     <div class="">
-      Powered by <a href="https://github.com/openzim/nautilus" class="link-secondary">Nautilus</a> &
-      <a href="https://kiwix.org" class="link-secondary">Kiwix</a>
+      Powered by <a href="https://github.com/openzim/nautilus" target="_blank" class="link-secondary">Nautilus</a> &
+      <a href="https://kiwix.org" target="_blank" class="link-secondary">Kiwix</a>
     </div>
     <div class="">Copyright {{ new Date().getFullYear() }}</div>
   </footer>
 </template>
+
+<style type="text/css">
+  .link-secondary:hover {
+    cursor: pointer;
+  }
+</style>
