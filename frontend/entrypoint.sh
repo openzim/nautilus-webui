ae584e430293a3343ace5d46b6c68c2e3ec6de0a
renaud gaudin <reg@rskg.org>
2024-05-09 16:21:56 +0000

    use same indent level as prettier

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,7 +3,7 @@
 JS_PATH=/user/share/nginx/html/environ.json
 echo "dump NAUTILUS_* environ variables to $JS_PATH"
 
-python3 -c 'import os; import json; print(json.dumps({k: v for k, v in os.environ.items() if k.startswith("NAUTILUS_")}, indent=4))' > $JS_PATH
+python3 -c 'import os; import json; print(json.dumps({k: v for k, v in os.environ.items() if k.startswith("NAUTILUS_")}, indent=2))' > $JS_PATH
 
 cat $JS_PATH
 echo "-----"
