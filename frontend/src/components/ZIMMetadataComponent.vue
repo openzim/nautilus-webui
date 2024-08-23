b22d7b54c804ccf1432ab72c82d5dd082db8737f
renaud gaudin <reg@rskg.org>
2024-08-23 18:17:27 +0000

    order meta same as in edit

--- frontend/src/components/ZIMMetadataComponent.vue
+++ frontend/src/components/ZIMMetadataComponent.vue
@@ -31,13 +31,13 @@
     </div>
     <form :onsubmit="eatEvent">
     <div class="row" v-if="inMetadataEditMode">
-      <p><input class="form-control" type="text" name="zimtitle" required v-model="archiveMetadataFormModal.title" placeholder="ZIM Title metadata" /></p>
-      <p><input class="form-control" type="text" name="zimdescription" required v-model="archiveMetadataFormModal.description" placeholder="ZIM Description metadata" /></p>
-      <p><input class="form-control" type="text" name="zimname" required v-model="archiveMetadataFormModal.name" placeholder="ZIM Name metadata" /></p>
+      <p><input class="form-control" type="text" name="zim-title" required v-model="archiveMetadataFormModal.title" placeholder="ZIM Title metadata" /></p>
+      <p><input class="form-control" type="text" name="zim-description" required v-model="archiveMetadataFormModal.description" placeholder="ZIM Description metadata" /></p>
+      <p><input class="form-control" type="text" name="zim-name" required v-model="archiveMetadataFormModal.name" placeholder="ZIM Name metadata" /></p>
       <p>Illustration (48x48px PNG): <ImageUpload :value="archiveMetadataFormModal.illustration" enforced_format="png" :enforced_dimensions="{width: 48, height: 49}" style="width: 48px; height: 48px;" @change="onIllustrationUpdate" /></p>
       <p>Main logo (horizontal PNG): <ImageUpload :value="archiveMetadataFormModal.main_logo" enforced_format="png" style="height: 4rem; max-width: 24rem;" @change="onMainlogoUpdate" /></p>
-      <p><input class="form-control" type="text" name="zimcreator" required v-model="archiveMetadataFormModal.creator" placeholder="ZIM Creator metadata" /></p>
-      <p><input class="form-control" type="text" name="zimlang" required v-model="archiveMetadataFormModal.languages" placeholder="ZIM Language metadata" /></p>
+      <p><input class="form-control" type="text" name="zim-creator" required v-model="archiveMetadataFormModal.creator" placeholder="ZIM Creator metadata" /></p>
+      <p><input class="form-control" type="text" name="zim-lang" required v-model="archiveMetadataFormModal.languages" placeholder="ZIM Language metadata" /></p>
       <p><smart-tagz
             autosuggest
             editable
@@ -48,19 +48,18 @@
             :allowDuplicates="false"
         />
       </p>
-      <p><input class="form-control" type="text" required name="zimfilename" v-model="archiveMetadataFormModal.filename" placeholder="ZIM Filename" /></p>
+      <p><input class="form-control" type="text" required name="zim-filename" v-model="archiveMetadataFormModal.filename" placeholder="ZIM Filename" /></p>
     </div>
     <div class="row" v-else>
       <div>Title: {{ archiveMetadataFormModal.title }}</div>
       <div>Description: {{ archiveMetadataFormModal.description }}</div>
       <div>Name: {{ archiveMetadataFormModal.name }}</div>
+      <div>Illustration: <img :src="'data:image/png;base64,' + archiveMetadataFormModal.illustration" class="rounded-2 border-0 illustration" /></div>
+      <div>Logo: <img :src="'data:image/png;base64,' + archiveMetadataFormModal.main_logo" class="border-0 main-logo"/></div>
       <div>Creator: {{ archiveMetadataFormModal.creator }}</div>
       <div>Language: {{ archiveMetadataFormModal.languages }}</div>
       <div>Tags: <span v-for="(tag) in archiveMetadataFormModal.tags" class="badge rounded-pill text-bg-secondary me-1">{{ tag }}</span></div>
-      <div>Filename: {{ archiveMetadataFormModal.filename }}</div>
-      <div>Name: {{ archiveMetadataFormModal.name }}</div>
-      <div>Logo: <img :src="'data:image/png;base64,' + archiveMetadataFormModal.main_logo" class="border-0 main-logo"/></div>
-      <div>Illustration: <img :src="'data:image/png;base64,' + archiveMetadataFormModal.illustration" class="rounded-2 border-0 illustration" /></div>
+      <div>Filename: {{ archiveMetadataFormModal.filename }}</div>    
     </div>
     <p><input class="form-control" type="email" v-model="archiveMetadataFormModal.email" placeholder="What's your email?" name="email" /></p>
     <p><button class="btn btn-primary" :disabled="inMetadataEditMode" @click.prevent="requestZIM">ZIM it!</button></p>
